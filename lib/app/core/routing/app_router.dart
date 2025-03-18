ba709b2f34a27cb59f7b9d7cf03e92eb0302a380
fady mohamed abo alhassan <132343355+Fady4Mohamed@users.noreply.github.com>
2025-03-18 23:52:39 +0200

    edit on navigation in azker

--- lib/app/core/routing/app_router.dart
+++ lib/app/core/routing/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:muslim/app/featuers/azkar/presentation/ui/azkar_page.dart';
 import 'package:muslim/app/featuers/home/presentation/home_view.dart';
 import 'package:muslim/app/featuers/qibla/presentation/qiblah_view.dart';
 import 'package:muslim/app/shared/navigation_bar.dart';
@@ -7,6 +8,7 @@ class AppRouter {
   static const pageNavBar = '/pageNavBar';
   static const homeView = '/homeView';
   static const qiblaView = '/qiblaView';
+  static const azkarPage = '/azkarPage';
 
   static final router = GoRouter(
     initialLocation: pageNavBar,
@@ -22,6 +24,10 @@ class AppRouter {
       GoRoute(
         path: qiblaView,
         builder: (context, state) => const QiblahView(),
+      ),
+      GoRoute(
+        path: azkarPage,
+        builder: (context, state) => const AzkarPage(),
       ),
     ],
   );
